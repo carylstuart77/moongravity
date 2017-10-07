d39b6a19df66e51aeee954d2ca2bf62384cb0861
Caryl Stuart <carylstuart77@gmail.com>
2017-10-07 07:27:00 -0600

    chp 1 p 616

---
+++ src/edu/cnm/deepdive/book/MoonGravity.java
@@ -0,0 +1,17 @@
+package edu.cnm.deepdive.book;
+
+public class MoonGravity {
+  public static void main(String args[])
+  {
+    double earthweight;
+    double moonweight;
+
+    earthweight = 130;
+
+    moonweight = earthweight * 0.17;
+
+    System.out.println(earthweight + " earth-pounds is equivalant to " + moonweight + " moons-pounds.");
+
+  }
+
+}
